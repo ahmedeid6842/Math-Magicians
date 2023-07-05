f5e23715b14f3cf97eb4c348f7e3663bcc29aa16
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-05 22:32:16 +0300

    Fixed eslint error

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,5 +1,7 @@
-import { Component } from "react";
-import { Row, Col, Button, Form } from "react-bootstrap";
+import { Component } from 'react';
+import {
+  Row, Col, Button, Form,
+} from 'react-bootstrap';
 
 class Calculator extends Component {
   constructor(props) {
