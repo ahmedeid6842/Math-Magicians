31efffe6ca6b275fbcedb872847ea3e68f7ab19b
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-06 01:05:30 +0300

    Convert the Class components to functional

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Calculator from './components/calculator';
+
+function App() {
+  return <Calculator />;
+}
+
+export default App;
+
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,136 +1,75 @@
-import { Component } from 'react';
 import {
   Row, Col, Button, Form,
 } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function ButtonPanel({ variant, text }) {
+  return (
+    <Col>
+      <Button
+        className="w-100 p-4 border-secondary"
+        variant={`${variant}`}
+      >
+        {text}
+      </Button>
+    </Col>
+  );
+}
 
-  render() {
-    return (
-      <div className="container">
-        <Row className="m-3">
-          <Col>
-            <Form.Control
-              dir="rtl"
-              className="bg-secondary text-right"
-              readOnly
-              value="0"
-              size="lg"
-            />
-          </Col>
-        </Row>
-        <Row className="m-3">
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              AC
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              +/-
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              %
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="warning">
-              /
-            </Button>
-          </Col>
-        </Row>
-        <Row className="m-3">
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              7
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              8
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              9
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="warning">
-              x
-            </Button>
-          </Col>
-        </Row>
-        <Row className="m-3">
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              4
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              5
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              6
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="warning">
-              -
-            </Button>
-          </Col>
-        </Row>
-        <Row className="m-3">
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              1
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              2
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              3
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="warning">
-              +
-            </Button>
-          </Col>
-        </Row>
-        <Row className="m-3">
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              0
-            </Button>
-          </Col>
-          <Col xs={6}>
-            <Button className="w-100 p-4 border-secondary" variant="light">
-              .
-            </Button>
-          </Col>
-          <Col>
-            <Button className="w-100 p-4 border-secondary" variant="warning">
-              =
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+ButtonPanel.propTypes = {
+  variant: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+function Calculator() {
+  return (
+    <div className="container">
+      <Row className="m-3">
+        <Col>
+          <Form.Control
+            dir="rtl"
+            className="bg-secondary text-right"
+            readOnly
+            value="0"
+            size="lg"
+          />
+        </Col>
+      </Row>
+      <Row className="m-3">
+        <ButtonPanel variant="light" text="AC" />
+        <ButtonPanel variant="light" text="+/-" />
+        <ButtonPanel variant="light" text="%" />
+        <ButtonPanel variant="warning" text="/" />
+      </Row>
+      <Row className="m-3">
+        <ButtonPanel variant="light" text="7" />
+        <ButtonPanel variant="light" text="8" />
+        <ButtonPanel variant="light" text="9" />
+        <ButtonPanel variant="warning" text="x" />
+      </Row>
+      <Row className="m-3">
+        <ButtonPanel variant="light" text="4" />
+        <ButtonPanel variant="light" text="5" />
+        <ButtonPanel variant="light" text="6" />
+        <ButtonPanel variant="warning" text="-" />
+      </Row>
+      <Row className="m-3">
+        <ButtonPanel variant="light" text="1" />
+        <ButtonPanel variant="light" text="2" />
+        <ButtonPanel variant="light" text="3" />
+        <ButtonPanel variant="warning" text="+" />
+      </Row>
+      <Row className="m-3">
+        <Col xs={6}>
+          <Button className="w-100 p-4 border-secondary" variant="light">
+            0
+          </Button>
+        </Col>
+        <ButtonPanel variant="light" text="." />
+        <ButtonPanel variant="warning" text="=" />
+      </Row>
+    </div>
+  );
 }
 
 export default Calculator;
