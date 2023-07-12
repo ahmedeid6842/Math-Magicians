25c519452fe3e426669201442cac42f57658c995
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-12 03:50:07 +0300

    Added basic route setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home';
 import Calculator from './components/calculator';
 import QuoteDisplay from './components/quote';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <QuoteDisplay />
-    </div>
+    <main>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<QuoteDisplay />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ src/components/home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <section className="container home-conainter">
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        euismod, lectus quis fermentum bibendum, mauris ipsum tristique dui, nec
+        molestie nulla augue vel felis. Donec at convallis felis. Nunc auctor,
+        elit nec vulputate maximus, mi massa faucibus ipsum, vel sodales nisl
+        quam ut quam. Sed luctus arcu in elit ullamcorper, eu sagittis leo
+        pulvinar. Quisque ullamcorper, nulla sit amet vestibulum commodo, tellus
+        augue aliquet libero, vitae tincidunt mauris sapien in enim. Integer ac
+        nisl vel eros laoreet ultrices quis vel augue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis
+        leo at massa tincidunt lacinia. Pellentesque lacinia, nulla vel lacinia
+        consequat, risus nulla condimentum diam, non finibus sapien eros vitae
+        nisl. Nulla facilisi. Fusce aliquet, turpis eu fringilla bibendum, velit
+        sapien aliquam elit, et molestie magna felis vel tortor. Sed euismod,
+        quam et laoreet eleifend, quam nisi scelerisque velit, quis pulvinar
+        arcu enim ut metus. Aliquam ac purus sed nisl maximus imperdiet. Nullam
+        sed tristique risus.
+      </p>
+    </section>
+  </>
+);
+
+export default Home;
